689b99c5366efa1b87a99d34d1b0bd15a8ccd81c
Pandamilla <linns@hochschule-trier.de>
2025-03-19 20:55:27 +0100

    SteuerungsInfos als Asset in den Szenen platziert

--- too_many_spells/components/bark/Bark.cs
+++ too_many_spells/components/bark/Bark.cs
@@ -3,46 +3,46 @@ using System;
 
 public partial class Bark : Node2D
 {
-    private Dialogbox _dialogbox = null!;
-    private Timer _barkTimer = null!;
+	private Dialogbox _dialogbox = null!;
+	private Timer _barkTimer = null!;
 
-    public override void _Ready()
-    {
-        _dialogbox = GetNode<Dialogbox>("Barkbox");
-        _barkTimer = GetNode<Timer>("BarkTimer");
+	public override void _Ready()
+	{
+		_dialogbox = GetNode<Dialogbox>("Barkbox");
+		_barkTimer = GetNode<Timer>("BarkTimer");
 
-        _dialogbox.TalkingPointsFinished += OnTalkingPointsFinished;
+		_dialogbox.TalkingPointsFinished += OnTalkingPointsFinished;
 
-        _barkTimer.OneShot = true;
-        _barkTimer.Timeout += OnBarkTimerTimeout;
-    }
+		_barkTimer.OneShot = true;
+		_barkTimer.Timeout += OnBarkTimerTimeout;
+	}
 
-    public void Enable()
-    {
-        GD.Print("Bark enabled");
+	public void Enable()
+	{
+		GD.Print("Bark enabled");
 
-        var timerInterval = GD.RandRange(10, 35);
-        GD.Print($"Bark timer interval: {timerInterval}");
-        _barkTimer.Start(timerInterval);
-    }
+		var timerInterval = GD.RandRange(10, 35);
+		GD.Print($"Bark timer interval: {timerInterval}");
+		_barkTimer.Start(timerInterval);
+	}
 
-    public void Disable()
-    {
-        GD.Print("Bark disabled");
-        _barkTimer.Stop();
-    }
+	public void Disable()
+	{
+		GD.Print("Bark disabled");
+		_barkTimer.Stop();
+	}
 
-    private void OnBarkTimerTimeout()
-    {
-        GD.Print("Bark timer timeout");
+	private void OnBarkTimerTimeout()
+	{
+		GD.Print("Bark timer timeout");
 
-        Barks.Bark bark = Barks.Instance.GetRandomBark();
+		Barks.Bark bark = Barks.Instance.GetRandomBark();
 
-        _dialogbox.Talk(bark.TalkingPoints, bark.SpeakerName);
-    }
+		_dialogbox.Talk(bark.TalkingPoints, bark.SpeakerName);
+	}
 
-    private void OnTalkingPointsFinished()
-    {
-        this.Enable();
-    }
+	private void OnTalkingPointsFinished()
+	{
+		this.Enable();
+	}
 }
